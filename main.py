582723c605d4c7261694948bf775526dbf4e1dd0
Ana Pinto <anapinto2503@gmail.com>
2025-07-13 10:09:05 -0500

    feat: Add database connection

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Annotated
 
-app = FastAPI()
+from fastapi import FastAPI, Depends
+from sqlmodel import Session
+
+from database import create_db_and_tables, get_session
+
+
+@asynccontextmanager
+async def lifespan(app):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+SessionDep = Annotated[Session, Depends(get_session)]
+
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, world!"}
+    return {"message": "Hello, World!"}
